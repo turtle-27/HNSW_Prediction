7c8ab52f9e551fafa1bb9db45c02129ee598998e
Prateek Chaudhury <58587461+Prat1510@users.noreply.github.com>
2022-03-22 21:10:51 +0530

    Update HNSW.cpp

--- HNSW.cpp
+++ HNSW.cpp
@@ -2,50 +2,46 @@
 #include <stack>
 #include <set>
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define max_stack_size 5
+#define k 5
 
-double cosine_dist(vector<double> a, vector<double> b)
+double cosine_dist(vector<double> A, vector<double> B)
 {
-    int size = a.size();
-    double out;
-    if(size != b.size())
+    if(A.size() != B.size())
     {
         cout << "Dimension mismatch!" << endl;
     }
-    // calculate cosine_dist
-}
 
-int findMax(stack<int> s)
-{
-    int m = s.top(); //initialization
-
-    int a;
-
-    while (!s.empty())
-    {
-        a = s.top();
-
-        if (m < a)
-            m = a; //Storing the maximum element in m
-
-        s.pop(); //removing the topmost element to bring next element at the top
+    double dot = 0.0, denom_a = 0.0, denom_b = 0.0 ;
+    for(unsigned int i = 0u; i < A.size(); i++) {
+        dot += A[i] * B[i] ;
+        denom_a += A[i] * A[i] ;
+        denom_b += B[i] * B[i] ;
     }
-
-    return m;
+    return 1.0 - dot / (sqrt(denom_a) * sqrt(denom_b));
 }
 
-stack<<pair<int, double>> SearchLayer(vector<double> q, stack<<pair<int, double>> candidates,vector<int> indptr,vector<int> index, vector<int> level_offset,int lc, set<int> visited,vector<vector<double>> vect)
+void trim(priority_queue<pair<int, double>> top_k, int k)
 {
-    stack<<pair<int, double>> top_k(candidates);
+    while(top_k.size() > k)
+    {
+        top_k.pop();
+    }
+    return;
+}
+
+priority_queue<pair<int, double>> SearchLayer(vector<double> q, priority_queue<pair<int, double>> candidates,vector<int> indptr,vector<int> index, vector<int> level_offset, int lc, set<int> visited,vector<vector<double>> vect, int k)
+{
+    priority_queue<pair<int, double>> top_k(candidates);
     while (candidates.size() > 0)
     {
         int ep = candidates.top();
         candidates.pop();
         int start = indptr[ep] + level_offset[lc];
         int end = indptr[ep] + level_offset[lc+1];
-        for(int i = start; i< end; i++)
+        for(int i = start; i < end; i++)
         {
             int px = index[i];
             if(visited.find(px) != visited.end() || px == -1)
@@ -54,12 +50,28 @@ stack<<pair<int, double>> SearchLayer(vector<double> q, stack<<pair<int, double>
             }
             visited.insert(px);
             int _dist = cosine_dist(q, vect[px]);
-            if(_dist > findMax(top_k) and top_k.size() == max_stack_size)
+            if(_dist > top_k.top().first and top_k.size() == k)
             {
                 continue;
             }
-            top_k.push()
+            top_k.push({ _dist, px});
+            trim(top_k, k);
+            candidates.push({ _dist, px});
         }
     }
-    
+    return top_k;
+}
+
+
+priority_queue<pair<int, double>> QueryHNSW(vector<int> q, int topk, int ep, vector<int> indptr, vector<int> index, vector<int> level_offset, int max_level, vector<vector<double>> vect, int k)
+{
+    priority_queue<pair<double, int>> top_k;
+    top_k.push({cosine_dist(q, vect[ep]), ep});
+    unordered_map<int, bool> visited;
+    visited[ep] = true;
+    for (int level = max_level; level >= 0; level--)
+    {
+        top_k = SearchLayer(q, top_k, indptr, index, level_offset, level, visited, vect, k);
+    }
+    return top_k;
 }
